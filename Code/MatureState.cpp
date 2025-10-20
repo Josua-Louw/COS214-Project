5305672be2be090f2a1d903c31a832aa29b955ee
DraiusErasmus <u24586189@tuks.ca.za>
2025-10-20 11:38:19 +0200

    Added MatureState doxygen.

---
+++ Code/MatureState.cpp
@@ -0,0 +1,15 @@
+#include "MatureState.h"
+
+void MatureState::handleCare() {
+    // Implement care actions specific to the mature state
+
+    // TODO - implement MatureState::handleCare
+    throw "Not yet implemented";
+}
+
+void MatureState::transitionToNext() {
+    // Implement the logic to transition the plant to the next state
+
+    // TODO - implement MatureState::transitionToNext
+    throw "Not yet implemented";
+}
---
+++ Code/MatureState.h
@@ -0,0 +1,26 @@
+#ifndef MATURESTATE_H
+#define MATURESTATE_H
+/**
+ * @file MatureState.h
+ * @brief This file contains the definition of the MatureState class.
+ */
+
+/**
+* @class MatureState
+* @brief Represents the plant being in the mature state of its lifetime.
+*/
+class MatureState {
+public:
+
+    /**
+    * @brief Handles the care actions specific to the mature state.
+    */
+    void handleCare();
+
+    /**
+    * @brief Transitions the plant to the next state in its lifecycle.
+    */
+    void transitionToNext();
+};
+
+#endif // MATURESTATE_H
