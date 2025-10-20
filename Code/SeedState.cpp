2ef296e731e52ecbb26f3cc2e704399c64a7bb40
DraiusErasmus <u24586189@tuks.ca.za>
2025-10-20 11:23:29 +0200

    Added SeedState doxygen.

--- Code/SeedState.cpp
+++ Code/SeedState.cpp
@@ -1,11 +1,15 @@
 #include "../Visual Paradigm files/SeedState.h"
 
 void SeedState::handleCare() {
+	// Implement care actions specific to the seed state
+
 	// TODO - implement SeedState::handleCare
 	throw "Not yet implemented";
 }
 
 void SeedState::transitionToNext() {
+	// Implement the logic to transition the plant to the next state
+
 	// TODO - implement SeedState::transitionToNext
 	throw "Not yet implemented";
 }
--- Code/SeedState.h
+++ Code/SeedState.h
@@ -1,12 +1,27 @@
 #ifndef SEEDSTATE_H
 #define SEEDSTATE_H
 
+/**
+* @file SeedState.h
+* @brief This file contains the definition of the SeedState class.
+*/
+
+/**
+* @class SeedState
+* @brief Represents the plant being in the seed state of its lifetime.
+*/
 class SeedState : Class_Diagram,_State_and_Prototype::PlantState {
 
-
 public:
+
+	/**
+	* @brief Handles the care actions specific to the seed state.
+	*/
 	void handleCare();
 
+	/**
+	* @brief Transitions the plant to the next state in its lifecycle.
+	*/
 	void transitionToNext();
 };
 
