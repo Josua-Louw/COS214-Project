afe0ebb5967230c7b254b5b5a87f7be2951fb638
Nico-Theron <u23565722@tuks.co.za>
2025-10-20 18:48:29 +0200

    Added Doxygen documentation for all Strategy pattern classes

--- Code/FertilizerBoostStrategy.cpp
+++ Code/FertilizerBoostStrategy.cpp
@@ -1,10 +1,32 @@
 #include "../Visual Paradigm files/FertilizerBoostStrategy.h"
 
+/**
+ * @file FertilizerBoostStrategy.cpp
+ * @brief Implementation of the FertilizerBoostStrategy class.
+ *
+ * Contains the definitions for applying fertilizer-based care to a plant
+ * and retrieving the strategy’s identifying name.
+ */
+
+/**
+ * @brief Applies fertilizer-based care to the plant.
+ *
+ * @note This method is part of the Strategy pattern and defines
+ * how this specific care approach benefits the plant.
+ *
+ * @todo Implement the logic to apply fertilizer effects to the plant’s state.
+ */
 void FertilizerBoostStrategy::applyCare() {
 	// TODO - implement FertilizerBoostStrategy::applyCare
 	throw "Not yet implemented";
 }
 
+/**
+ * @brief Returns the identifying name of this strategy.
+ *
+ * @note Used for debugging or UI purposes to indicate which
+ * care strategy is currently active.
+ */
 void FertilizerBoostStrategy::getStrategyName() {
 	// TODO - implement FertilizerBoostStrategy::getStrategyName
 	throw "Not yet implemented";
