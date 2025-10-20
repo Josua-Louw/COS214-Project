c79bd1c69657ce24284c7d3296addea2b009ecaf
DraiusErasmus <u24586189@tuks.ca.za>
2025-10-20 12:03:51 +0200

    Added SenescenceState doxygen.

--- Code/SenescenceState.cpp
+++ Code/SenescenceState.cpp
@@ -1,11 +1,15 @@
 #include "../Visual Paradigm files/SenescenceState.h"
 
 void SenescenceState::handleCare() {
+	// Implement care actions specific to the senescence state
+
 	// TODO - implement SenescenceState::handleCare
 	throw "Not yet implemented";
 }
 
 void SenescenceState::transitionToNext() {
+	// Implement the logic to transition the plant to the next state
+
 	// TODO - implement SenescenceState::transitionToNext
 	throw "Not yet implemented";
 }
--- Code/SenescenceState.h
+++ Code/SenescenceState.h
@@ -1,12 +1,26 @@
 #ifndef SENESCENCESTATE_H
 #define SENESCENCESTATE_H
+/**
+ * @file SenescenceState.h
+ * @brief This file contains the definition of the SenescenceState class.
+ */
 
+/**
+ * @class SenescenceState
+ * @brief Represents the plant being in the senescence state of its lifetime.
+ */
 class SenescenceState : Class_Diagram,_State_and_Prototype::PlantState {
 
-
 public:
+
+	/**
+	 * @brief Handles the care actions specific to the senescence state.
+	 */
 	void handleCare();
 
+	/**
+	 * @brief Transitions the plant to the next state in its lifecycle.
+	 */
 	void transitionToNext();
 };
 
