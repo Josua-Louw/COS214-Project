e578544bfc6f971e3aed0a82adc6d243366945ce
DraiusErasmus <u24586189@tuks.ca.za>
2025-10-20 11:54:58 +0200

    Added FloweringState doxygen.

--- Code/FloweringState.cpp
+++ Code/FloweringState.cpp
@@ -1,11 +1,15 @@
 #include "../Visual Paradigm files/FloweringState.h"
 
 void FloweringState::handleCare() {
+	// Implement care actions specific to the flowering state
+
 	// TODO - implement FloweringState::handleCare
 	throw "Not yet implemented";
 }
 
 void FloweringState::transitionToNext() {
+	// Implement the logic to transition the plant to the next state
+
 	// TODO - implement FloweringState::transitionToNext
 	throw "Not yet implemented";
 }
--- Code/FloweringState.h
+++ Code/FloweringState.h
@@ -1,12 +1,26 @@
 #ifndef FLOWERINGSTATE_H
 #define FLOWERINGSTATE_H
+/**
+ * @file FloweringState.h
+ * @brief This file contains the definition of the FloweringState class.
+ */
 
+/**
+* @class FloweringState
+* @brief Represents the plant being in the flowering state of its lifetime.
+*/
 class FloweringState : Class_Diagram,_State_and_Prototype::PlantState {
 
-
 public:
+
+	/**
+	* @brief Handles the care actions specific to the flowering state.
+	*/
 	void handleCare();
 
+	/**
+	* @brief Transitions the plant to the next state in its lifecycle.
+	*/
 	void transitionToNext();
 };
 
