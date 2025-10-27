7171fbd7a66305786c40f6eee3aeb257289fbd89
Nico-Theron <u23565722@tuks.co.za>
2025-10-27 13:55:06 +0200

    Adjusted Command.h to prepare for aborting commands

--- Code/Command.h
+++ Code/Command.h
@@ -1,5 +1,10 @@
 #ifndef COMMAND_H
 #define COMMAND_H
+#include <string>
+#include <atomic>
+#include <vector>
+#include <thread>
+#include <chrono>
 
 /**
  * @class Command
@@ -8,7 +13,16 @@
  * Encapsulates staff tasks (e.g., watering, fertilizing, selling) as command objects, allowing tasks to be stored, executed, and potentially undone. Commands are assigned to Staff via NurseryHub (Mediator) and stored in Staff's taskList. This pattern decouples task requests from their execution, enabling flexible task management.
  */
 class Command {
+protected:
+    std::atomic<bool> abort{false};
 public:
+    bool getAbortStatus() {
+        return abort;
+    };
+
+    void setAbortStatus(bool abort) {
+        this->abort.store(abort, std::memory_order_release);
+    };
     /**
      * @brief Executes the command, performing the specific task.
      * 
