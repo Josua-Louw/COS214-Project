a24bde572c821905cc51c39d439ec1f027cb0db0
Fishynastix <heindrichjansen@gmail.com>
2025-10-28 09:54:55 +0200

    Added doxygen comments to AddDecoration.h, AddPlant.h and AddPot.h.

--- Code/AddDecoration.h
+++ Code/AddDecoration.h
@@ -21,10 +21,25 @@ class AddDecoration : public OrderBuilder {
 
 
 public:
+	/**
+	 * @brief Constructor for AddDecoration.
+	 * @param gh Pointer to the GreenHouse instance.
+	 */
 	AddDecoration(GreenHouse* gh);
 
+	/**
+	 * @brief Builds the part of the order by adding decorative plants based on the item name.
+	 * @param order Pointer to the current order.
+	 * @param itemName Name of the decorative plant item to be added.
+	 * @return Pointer to the updated order.
+	 */
 	Order* buildPart(Order* order, std::string itemName);
 
+	/**
+	 * @brief Checks if the given plant is of decorative type.
+	 * @param item Pointer to the plant to be checked.
+	 * @return true if the plant is decorative, false otherwise.
+	 */
 	bool checkType(Item* item);
 };
 
