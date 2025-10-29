d6f9dad168772a2fda1f1df8a03000f0580b6a1b
DraiusErasmus <u24586189@tuks.ca.za>
2025-10-29 08:53:26 +0200

    Implemented alive variable

--- Code/DeadState.cpp
+++ Code/DeadState.cpp
@@ -6,6 +6,9 @@
 // Implement the logic to transition the plant to the next state
 void DeadState::transitionToNext() {
 	std::cout << "\033[1;32mDead start\033[0m " << plant_->getName() << std::endl;
+	if (plant_->getIsAlive() == false) {
+		plant_->killPlant();
+	};
 }
 
 DeadState::DeadState(GreenHousePlant * plant) : PlantState(plant) {
