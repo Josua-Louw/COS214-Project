72ec8b6623eb85f6bf1633f22bb80c86e847d085
Josua Louw <josuaperoldlouw@gmail.com>
2025-10-30 09:25:56 +0200

    set up customer to be able to direct the building of it's order dynamically via the nursery mediator

--- Code/Customer.cpp
+++ Code/Customer.cpp
@@ -1,6 +1,41 @@
 #include "Customer.h"
 #include "OrderBuilder.h"
 
+Customer::Customer(std::string id, NurseryMediator* mediator, GreenHouse* gh)
+	: Person(id), nurseryHub(mediator) {
+	// Create a random set of order builders
+	int numBuilders = rand() % 5 + 1; // Between 1 and 5 builders
+	int seedPacketsInDecoratedOrder = 0;
+	bool addedNewPlant = false;
+	for (int i = 0; i < numBuilders; ++i) {
+		// Randomly choose a builder type to add
+		int builderType = rand() % 4;
+		switch (builderType) {
+		case 0:
+			if (addedNewPlant && seedPacketsInDecoratedOrder >= 1) {
+				// Skip adding another seed packet if limit reached
+				continue;
+			}
+			orderBuilders.push_back(new AddSeed(gh));
+			seedPacketsInDecoratedOrder = (addedNewPlant) ? seedPacketsInDecoratedOrder + 1 : 0;
+			break;
+		case 1:
+			orderBuilders.push_back(new AddPot(gh));
+			break;
+		case 2:
+			orderBuilders.push_back(new AddDecoration(gh));
+			break;
+		case 3:
+			orderBuilders.push_back(new AddPlant(gh));
+			seedPacketsInDecoratedOrder = 0;
+			addedNewPlant = true;
+			break;
+		default:
+			break;
+		}
+	}
+}
+
 void Customer::buy() {
 	//Idea get list of available items from mediator according to the builder's type
 	//use some randomization to select items to add to the order
@@ -9,9 +44,16 @@ void Customer::buy() {
 	for (OrderBuilder* builder : orderBuilders) {
 		if (builder) {
 			//find item's of builder's type from mediator
-			order = builder->buildPart(order, itemNames.front());
-			//NOTE: itemNames is temporary for testing
-			itemNames.erase(itemNames.begin());
+			//TODO: implement requestItemList in mediator
+			std::vector<std::string> itemNames = nurseryHub->requestItemList(builder);
+			if (itemNames.empty()) {
+				continue;
+			}
+			//randomly select an item from the list
+			int randomIndex = rand() % itemNames.size();
+			std::string selectedItem = itemNames[randomIndex];
+			//build part of the order with the selected item
+			order = builder->buildPart(order, selectedItem);
 		}
 	}
 	order->printOrder();
--- Code/Customer.h
+++ Code/Customer.h
@@ -4,29 +4,80 @@
 #include "Person.h"
 #include "NurseryMediator.h"
 #include "Order.h"
+#include "AddSeed.h"
+#include "AddPot.h"
+#include "AddDecoration.h"
+#include "AddPlant.h"
 #include <vector>
+#include <cstdlib> // For rand() and srand()
 
 class OrderBuilder;
 
-class Customer : public Person {
+/**
+ * @file Customer.h
+ * @brief Declaration of the Customer class.
+ * @details The Customer class represents a customer in the nursery system.
+ * It inherits from the Person class and interacts with the NurseryMediator
+ * to place orders using various OrderBuilder strategies.
+ */
 
+/**
+ * @class Customer
+ * @brief Represents a customer who can place orders in the nursery system.
+ * @details The Customer class utilizes multiple OrderBuilder instances to
+ * create orders for different types of items available in the greenhouse.
+ * It communicates with the NurseryMediator to retrieve item lists and place orders.
+ * @note The Customer class is responsible for managing its own order builders
+ * and coordinating the order creation process.
+ * @note Represents the director in the Builder design pattern.
+ */
+
+class Customer : public Person {
 private:
+	// List of order builders used by the customer
 	std::vector<OrderBuilder*> orderBuilders;
+	// Mediator for communication with the nursery system
 	NurseryMediator* nurseryHub;
 
 	//TEMPORARY - for testing
 	std::vector<std::string> itemNames;
 
 public:
+	/**
+	 * @brief Initiates the buying process for the customer.
+	 * @details The method creates a new order and uses the assigned
+	 * OrderBuilder instances to add items to the order based on available items.
+	 */
 	void buy();
-
+	//Constructor to generate a random set of order builders
+	/**
+	 * @brief Constructs a Customer with a random set of OrderBuilders.
+	 * @param id Unique identifier for the customer.
+	 * @param mediator Pointer to the NurseryMediator instance.
+	 * @param gh Pointer to the GreenHouse instance, for the order builders.
+	 * @note The constructor randomly selects between 1 to 5 OrderBuilder types
+	 * to create a diverse order-building strategy for the customer.
+	 */
+	Customer(std::string id, NurseryMediator* mediator, GreenHouse* gh);
+	/**
+	 * @brief Constructs a Customer with specified OrderBuilders.
+	 * @param id Unique identifier for the customer.
+	 * @param mediator Pointer to the NurseryMediator instance.
+	 * @param builders Vector of OrderBuilder pointers to be used by the customer.
+	 * @note This constructor allows for explicit specification of the
+	 * OrderBuilder instances to be used for order creation.
+	 */
 	Customer(std::string id, NurseryMediator* mediator, std::vector<OrderBuilder*> builders)
 		: Person(id), nurseryHub(mediator), orderBuilders(builders) {};
 
 	//TEMPORARY CONSTRUCTOR FOR TESTING JUST TO ADD STRING VECTOR OF ITEMS TO ORDER
 	Customer(std::string id, NurseryMediator* mediator, std::vector<OrderBuilder*> builders, std::vector<std::string> itemNames)
 		: Person(id), nurseryHub(mediator), orderBuilders(builders), itemNames(itemNames) {};
-	
+	/**
+	 * @brief Destructor for the Customer class.
+	 * @details Cleans up dynamically allocated OrderBuilder instances.
+	 * @note Ensures proper memory management by deleting each OrderBuilder in the orderBuilders vector.
+	 */
 	virtual ~Customer();
 };
 
