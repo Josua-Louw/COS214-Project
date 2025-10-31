2b4c6e76cbcd418376f69ed026574a6f7cd6f873
Heindrich Jansen <heindrichjansen@gmail.com>
2025-10-31 15:23:30 +0200

    Made the GUI setup similar to the demoMain.

--- Code/Window.h
+++ Code/Window.h
@@ -2,16 +2,52 @@
 #define WINDOW_H
 
 #include <gtkmm.h>
+#include <vector>
 
 class GUISystemHandler;
 
 class Window : public Gtk::Window {
 public:
   Window(GUISystemHandler* guiSystem);
+  void updateStatus(const std::string& message);
+  void clearMainArea();
+  Gtk::Box* getMainBox() { return &m_main_box; }
+
 protected:
-  void on_button_click();
-  Gtk::Button m_button;
+  void setupMainMenu();
+  void setupPlantManagement();
+  void setupStaffManagement();
+  void setupOrderProcessing();
+  void setupGreenhouseView();
+
+  // Signal handlers
+  void on_main_menu_clicked();
+  void on_plant_management_clicked();
+  void on_staff_management_clicked();
+  void on_order_processing_clicked();
+  void on_greenhouse_view_clicked();
+
   GUISystemHandler* m_gui_system_handler;
+  
+  // Main layout
+  Gtk::Box m_main_box;
+  Gtk::Box m_header_box;
+  Gtk::Box m_content_box;
+  Gtk::Box m_footer_box;
+  
+  // Header widgets
+  Gtk::Label m_title_label;
+  Gtk::Button m_main_menu_btn;
+  Gtk::Button m_plant_mgmt_btn;
+  Gtk::Button m_staff_mgmt_btn;
+  Gtk::Button m_order_btn;
+  Gtk::Button m_greenhouse_btn;
+  
+  // Content area (will be dynamically updated)
+  Gtk::Box* m_current_content;
+  
+  // Footer
+  Gtk::Label m_status_label;
 };
 
 #endif // WINDOW_H
