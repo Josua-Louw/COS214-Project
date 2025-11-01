9519a335b7071f8f241d12cf173bd85ae785c3dc
Heindrich Jansen <heindrichjansen@gmail.com>
2025-11-01 12:47:41 +0200

    Fixed the errors in window barring the GUI from working.

--- Code/Window.h
+++ Code/Window.h
@@ -9,9 +9,10 @@ class GUISystemHandler;
 class Window : public Gtk::Window {
 public:
   Window(GUISystemHandler* guiSystem);
+  virtual ~Window();
+  
   void updateStatus(const std::string& message);
   void clearMainArea();
-  Gtk::Box* getMainBox() { return &m_main_box; }
 
 protected:
   void setupMainMenu();
@@ -28,26 +29,26 @@ protected:
   void on_greenhouse_view_clicked();
 
   GUISystemHandler* m_gui_system_handler;
-  
-  // Main layout
-  Gtk::VBox m_main_box;
-  Gtk::HBox m_header_box;
-  Gtk::VBox m_content_box;
-  Gtk::HBox m_footer_box;
-  
-  // Header widgets
-  Gtk::Label m_title_label;
-  Gtk::Button m_main_menu_btn;
-  Gtk::Button m_plant_mgmt_btn;
-  Gtk::Button m_staff_mgmt_btn;
-  Gtk::Button m_order_btn;
-  Gtk::Button m_greenhouse_btn;
-  
+
+  // Main layout - ALL POINTERS
+  Gtk::VBox* m_main_box;
+  Gtk::HBox* m_header_box;
+  Gtk::VBox* m_content_box;
+  Gtk::HBox* m_footer_box;
+
+  // Header widgets - ALL POINTERS
+  Gtk::Label* m_title_label;
+  Gtk::Button* m_main_menu_btn;
+  Gtk::Button* m_plant_mgmt_btn;
+  Gtk::Button* m_staff_mgmt_btn;
+  Gtk::Button* m_order_btn;
+  Gtk::Button* m_greenhouse_btn;
+
   // Content area (will be dynamically updated)
   Gtk::Widget* m_current_content;
-  
-  // Footer
-  Gtk::Label m_status_label;
+
+  // Footer - POINTER
+  Gtk::Label* m_status_label;
 };
 
 #endif // WINDOW_H
