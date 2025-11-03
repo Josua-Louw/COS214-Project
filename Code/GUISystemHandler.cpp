ea4c4bfd222debd2db0d187553ecf720a413ffc4
Josua Louw <josuaperoldlouw@gmail.com>
2025-11-03 13:35:10 +0200

    updated gui system handler

--- Code/GUISystemHandler.cpp
+++ Code/GUISystemHandler.cpp
@@ -6,6 +6,9 @@ GUISystemHandler::GUISystemHandler() : m_window(nullptr) {
     // Initialize system components
     greenHouse = new Section("Main Greenhouse", 100);
     nurseryHub = new NurseryHub();
+    nurseryHub->setInventoryRoot(greenHouse);
+    customerIdCounter = 0;
+    staffIdCounter = 0;
 }
 
 GUISystemHandler::~GUISystemHandler() {
@@ -22,102 +25,88 @@ void GUISystemHandler::systemMenue() {
 
 // Plant Management
 void GUISystemHandler::addPlant() {
-    std::cout << "Adding plant to greenhouse" << std::endl;
-    // Implementation would create a plant and add to greenhouse
-    Plant* newPlant = new Plant("Rose Plant", 25.0);
-    greenHouse->expand(newPlant);
-}
-
-void GUISystemHandler::addPot() {
-    std::cout << "Adding pot to greenhouse" << std::endl;
-    PotAdapter* newPot = new PotAdapter("Ceramic Pot", 15.0);
-    greenHouse->expand(newPot);
-}
-
-void GUISystemHandler::addSeedPacket() {
-    std::cout << "Adding seed packet to greenhouse" << std::endl;
-    SeedPacketAdapter* newSeeds = new SeedPacketAdapter("Sunflower Seeds", 5.0);
-    greenHouse->expand(newSeeds);
-}
-
-void GUISystemHandler::addDecoration() {
-    std::cout << "Adding decoration to greenhouse" << std::endl;
-    DecorationAdapter* newDecor = new DecorationAdapter("Glitter Decoration", 8.0);
-    greenHouse->expand(newDecor);
+    std::cout << "Adding item to greenhouse" << std::endl;
+    switch ( m_window->getAddPlantState())
+    {
+    case addPlantState::PLANT:
+        int index = rand() % plantNames.size();
+        std::string plantName = plantNames[index].first;
+        double plantPrice = plantNames[index].second;
+        Plant* newPlant = new Plant(plantName, plantPrice);
+        greenHouse->expand(newPlant);
+        break;
+    case addPlantState::POT:
+        std::string potName = potNames[rand() % potNames.size()].first;
+        double potPrice = potNames[rand() % potNames.size()].second;
+        PotAdapter* newPot = new PotAdapter(potName, potPrice);
+        greenHouse->expand(newPot);
+        break;
+    case addPlantState::SEED:
+        std::string seedName = seedNames[rand() % seedNames.size()].first;
+        double seedPrice = seedNames[rand() % seedNames.size()].second;
+        SeedPacketAdapter* newSeed = new SeedPacketAdapter(seedName, seedPrice);
+        greenHouse->expand(newSeed);
+        break;
+    case addPlantState::DECORATION:
+        std::string decorName = decorationNames[rand() % decorationNames.size()].first;
+        double decorPrice = decorationNames[rand() % decorationNames.size()].second;
+        DecorationAdapter* newDecor = new DecorationAdapter(decorName, decorPrice);
+        greenHouse->expand(newDecor);
+        break;
+    default:
+        break;
+    }
+    
 }
 
 // Staff Management
 void GUISystemHandler::registerStaffMember() {
     // Default implementation
-    registerPlantCaretaker();
-}
-
-void GUISystemHandler::registerPlantCaretaker() {
-    std::cout << "Registering plant caretaker" << std::endl;
-    static int caretakerCount = 0;
-    std::string id = "PC" + std::to_string(++caretakerCount);
-    PlantCaretaker* caretaker = new PlantCaretaker(id, nurseryHub);
-    nurseryHub->registerStaff(caretaker);
-}
-
-void GUISystemHandler::registerSalesManager() {
-    std::cout << "Registering sales manager" << std::endl;
-    static int managerCount = 0;
-    std::string id = "SM" + std::to_string(++managerCount);
-    SalesManager* manager = new SalesManager(id, nurseryHub);
-    nurseryHub->registerStaff(manager);
-}
-
-void GUISystemHandler::assignCareTask() {
-    std::cout << "Assigning care task" << std::endl;
-    // Implementation would create a care command and assign via hub
-}
-
-void GUISystemHandler::assignSaleTask() {
-    std::cout << "Assigning sale task" << std::endl;
-    // Implementation would create a sale command and assign via hub
+    std::cout << "Registering staff member" << std::endl;
+    switch (m_window->getRegisterStaffState())
+    {
+    case registerStaffState::PLANT_CARETAKER:
+        {
+            std::string id = "Plant Caretaker:" + std::to_string(++staffIdCounter);
+            PlantCaretaker* caretaker = new PlantCaretaker(id, nurseryHub);
+            nurseryHub->registerStaff(caretaker);
+        }
+        break;
+    case registerStaffState::SALES_MANAGER:
+        {
+            std::string id = "Sales Manager:" + std::to_string(++staffIdCounter);
+            SalesManager* manager = new SalesManager(id, nurseryHub);
+            nurseryHub->registerStaff(manager);
+        }
+        break;
+    default:
+        break;
+    }
 }
 
 // Order Processing
 void GUISystemHandler::processCustomerOrder() {
     std::cout << "Processing customer order" << std::endl;
-    // Implementation would process the current order
-}
-
-void GUISystemHandler::createOrder() {
-    std::cout << "Creating new order" << std::endl;
-    // Implementation would initialize a new order
-}
-
-void GUISystemHandler::addPlantToOrder() {
-    std::cout << "Adding plant to order" << std::endl;
-    // Implementation would add plant to current order
-}
-
-void GUISystemHandler::addPotToOrder() {
-    std::cout << "Adding pot to order" << std::endl;
-    // Implementation would add pot to current order
-}
-
-void GUISystemHandler::addSeedToOrder() {
-    std::cout << "Adding seeds to order" << std::endl;
-    // Implementation would add seeds to current order
-}
-
-void GUISystemHandler::addDecorationToOrder() {
-    std::cout << "Adding decoration to order" << std::endl;
-    // Implementation would add decoration to current order
-}
-
-// Greenhouse Management
-void GUISystemHandler::waterAllPlants() {
-    std::cout << "Watering all plants" << std::endl;
-    // Implementation would water all plants in greenhouse
-}
-
-void GUISystemHandler::fertilizeAllPlants() {
-    std::cout << "Fertilizing all plants" << std::endl;
-    // Implementation would fertilize all plants in greenhouse
+    std::string customerId = "Customer:" + std::to_string(++customerIdCounter);
+    switch (m_window->getProcessOrderState())
+    {
+    case processOrderState::CUSTOMER_ORDER:
+        {
+            CustomerOrder* order = new CustomerOrder(customerId, nurseryHub);
+            nurseryHub->registerCustomerOrder(order);
+            order->processOrder();
+        }
+        break;
+    case processOrderState::SELF_ORDER:
+        {
+            std::vector<OrderBuilder*> builders = m_window->getOrderBuilders();
+            Customer* order = new Customer(customerId, nurseryHub, builders, greenHouse);
+            nurseryHub->registerCustomerOrder(order);
+            order->processOrder();
+        }
+    default:
+        break;
+    }
 }
 
 std::string GUISystemHandler::getGreenhouseSummary() {
