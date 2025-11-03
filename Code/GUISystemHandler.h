ef7f60c95a60f6fab425914e399d416e9cc21136
Josua Louw <josuaperoldlouw@gmail.com>
2025-11-03 13:38:16 +0200

    removed uneccassary functions

--- Code/GUISystemHandler.h
+++ Code/GUISystemHandler.h
@@ -39,22 +39,11 @@ public:
     
     // Staff management
     void registerStaffMember() override;
-    void registerPlantCaretaker();
-    void registerSalesManager();
-    void assignCareTask();
-    void assignSaleTask();
     
     // Order processing
     void processCustomerOrder() override;
-    void createOrder();
-    void addPlantToOrder();
-    void addPotToOrder();
-    void addSeedToOrder();
-    void addDecorationToOrder();
     
     // Greenhouse management
-    void waterAllPlants();
-    void fertilizeAllPlants();
     std::string getGreenhouseSummary();
     std::string getInventory();
     std::string getStaffInfo();
